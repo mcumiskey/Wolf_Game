5d3a7f65afd8fc2cbd3fc59403d0c97ec7b076f0
Miles <milescumiskey@gmail.com>
2019-10-22 11:52:37 -0400

    yarn should work? now using namespace

--- WolfAndFox/Assets/Scripts/InteractWithYarn.cs
+++ WolfAndFox/Assets/Scripts/InteractWithYarn.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//usign Yarn
+namespace Yarn.Unity.Example {
+
 public class InteractWithYarn : MonoBehaviour {
+        public GameObject player;
+        public GameObject dialogRunner;
 
-    public GameObject player;
-    public GameObject dialogRunner;
+        // Start is called before the first frame update
+        void Start(){
+            
+        }
 
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update(){
-        
-    }
-    void OnTriggerStay(Collider other) {
-        //if the player is touching the object but not holding it
-        if (other.gameObject.tag == ("Player") ) {
-            //tell the player they can pick it up
-            player.GetComponent<PlayerControl>().displayCenterText("Talk to the Cube with Z");
-            if(Input.GetKeyDown(KeyCode.Z)) {
-               dialogRunner.GetComponent<DialogueRunner>().StartDialogue();
+        // Update is called once per frame
+        void Update(){
+            
+        }
+        void OnTriggerStay(Collider other) {
+            //if the player is touching the object but not holding it
+            if (other.gameObject.tag == ("Player") ) {
+                //tell the player they can pick it up
+                player.GetComponent<PlayerControl>().displayCenterText("Talk to the Cube with Z");
+                if(Input.GetKeyDown(KeyCode.Z)) {
+                    //start dialouge 
+                    FindObjectOfType<DialogueRunner>().StartDialogue();
+                }
             }
         }
     }
